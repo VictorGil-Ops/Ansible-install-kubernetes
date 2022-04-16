0ed384a21b2bc8f2ef7976862329d76790c8d72a
VictorGil-Ops <victorgilasp@gmail.com>
2022-04-16 20:02:56 +0200

    main

---
+++ playbooks/scripts/check_kubelet.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+exist=$(docker system info | grep -i driver | grep -i cgroupfs)
+
+if [ -z "$exist" ]
+then 
+
+   echo "empty"
+
+else 
+
+    cp -af /tmp/daemon.json /etc/docker/daemon.json;
+    systemctl daemon-reload && systemctl restart docker
+fi
