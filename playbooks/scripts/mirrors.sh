db14648daa8961242153e058c0f0ce8d6332e29a
VictorGil-Ops <victorgilasp@gmail.com>
2022-04-16 15:46:01 +0200

    main

---
+++ playbooks/scripts/mirrors.sh
@@ -0,0 +1,4 @@
+﻿#!/bin/bash
+# repare centos repository
+sudo sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
+sudo sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
